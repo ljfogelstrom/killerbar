1fb78b791f740b40f3c9ccdf2a86dd99aa3c4bd7
ljfogelstrom <ljfogelstrom@protonmail.com>
2025-11-22 16:42:11 +0100

    main.c: Formatting

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 /* TODO:
- *      - (in the future) add multi-threading and wide-string support
+ *      - add wide-string support
  */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -59,21 +60,29 @@ static _Bool sflag, vflag;
 static void init(void)
 {
     int i, ret;
-    for (i = 0; i<length; i++) { /* initialize continuous blocks */
-        if (block[i].timerspec.it_interval.tv_sec || block[i].timerspec.it_interval.tv_nsec ) { /* yep */
+
+    for (i = 0; i<length; i++) /* initialize continuous blocks */
+    {
+        if (block[i].timerspec.it_interval.tv_sec || block[i].timerspec.it_interval.tv_nsec )
+	{
             if (!memcmp(&block[i].timerspec.it_value, &(time_t){ 0 }, sizeof(time_t)))
-                block[i].timerspec.it_value.tv_sec = 1;
-                /* default initial timer value */
+                block[i].timerspec.it_value.tv_sec = 1; /* default initial timer value */
+
         block[i].event.sigev_notify = SIGEV_SIGNAL;
         block[i].event.sigev_signo = SIGRTMIN + i;
+
         ret = timer_create(CLOCK_REALTIME, &block[i].event, &block[i].timer);
         if (ret) ERR(timer_create);
+
         ret = timer_settime(block[i].timer, 0, &block[i].timerspec, NULL);
         if (ret) ERR(timer_settime);
-        } else { /* initialize intermittent blocks */
+        }
+	else /* initialize intermittent blocks */
+	{
             block[i].event.sigev_notify = SIGEV_THREAD;
             block[i].event.sigev_notify_function = notify;
             block[i].event.sigev_value.sival_int = i;
+
             ret = timer_create(CLOCK_REALTIME, &block[i].event, &block[i].timer);
             if (ret) ERR(timer_create);
         }
@@ -115,7 +124,7 @@ int main(int argc, char *argv[])
 
     Display *dpy = XOpenDisplay(NULL);
     int screen = XDefaultScreen(dpy);
-    Window root = RootWindow(dpy, screen); /* these functions will always set errno for some reason */
+    Window root = RootWindow(dpy, screen);
 
     init();
 
@@ -125,32 +134,37 @@ int main(int argc, char *argv[])
 
     int signo;
     /* main event loop */
-    while (running) {
+    while (running)
+    {
         sigwait(&sigset, &signo);
         if (vflag) fprintf(stderr, "Signal received: %d\n", signo);
+
         int i = signo - SIGRTMIN;
+
         if (i < 0)
             handle_signal_std(signo);
         else if (i > length)
             fprintf(stderr, "Signal %d is unassigned\n", signo);
-        else {
+        else
+	{
             block[i].function(block[i].arg);
-            if (!block[i].timerspec.it_interval.tv_sec && !block[i].timerspec.it_interval.tv_nsec) { /* is intermittent */
+            if (!block[i].timerspec.it_interval.tv_sec && !block[i].timerspec.it_interval.tv_nsec) /* is intermittent */
                 timer_settime(block[i].timer, 0, &block[i].timerspec, NULL);
-            }
+
             sprintf(status[i], block[i].fmt, buffer);
             for (int j = length-1; j >= 0; j--) {
                 strcat(result, status[j]);
                 if (j && *status[j]) strcat(result, delimiter);
             }
+
             if (sflag || vflag) puts(result);
             else {
                 XStoreName(dpy, root, result);
                 XFlush(dpy);
             }
+
             memset(result, 0, 256);
-            memset(buffer, 0, 512); /* added this additional call so that the same
-                                     * buffer contents are not printed repeated times by accident */
+            memset(buffer, 0, 512); /* so that the same buffer contents are not printed repeated times by accident */
         }
     }
 
